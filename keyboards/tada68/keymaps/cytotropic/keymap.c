9f7edb82db15724db4041f1452aab79ddd99de5d
Chris Korenczuk <Chris@Chriss-MacBook-Pro-10.local>
2018-02-03 20:07:25 -0600

    updated cytotropic

--- keyboards/tada68/keymaps/cytotropic/keymap.c
+++ keyboards/tada68/keymaps/cytotropic/keymap.c
@@ -1,34 +1,26 @@
 #include "tada68.h"
 
 /* DO TO LIST
-- Mac Media keys
-- Mac Brightness keys
-- Win Media keys
-- Reset key
-- grave esc
-- backlight changes?
-
- */
+- Mac Screen Brightness keys
+CHECK - Mac Media keys
+CHECK - Win Media keys
+CHECK - Reset key
+CHECK - backlight changes
+*/
 
 
 // LAYERZ
 #define _MBL 0 // Mac Base Layer (_MBL)
 #define _WFL 1 // Windows Function Layer (_WFL)
+#define _FL  2 // Functional Key Layer
 
-// Tap Dance declarations
+//KEYZ
+#define _______ KC_TRNS // transparent key
+
+// Tap Dance
 enum {
-  TD_F1 = 0,
-  TD_F2,
-  TD_F3,
-  TD_F4,
-  TD_F5,
-  TD_F6,
-  TD_F7,
-  TD_F8,
-  TD_F9,
-  TD_F10,
-  TD_F11,
-  TD_F12
+  TDM_1 = 0, TDM_2, TDM_3, TDM_4, TDM_5, TDM_6, TDM_7, TDM_8, TDM_9, TDM_10, TDM_11, TDM_12,
+  TDW_1,     TDW_2, TDW_3, TDW_4, TDW_5, TDW_6, TDW_7, TDW_8, TDW_9, TDW_10, TDW_11, TDW_12
 };
 
 // Macros
@@ -43,11 +35,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
     case M_MAC:
         SEND_STRING(SS_TAP(X_SCROLLLOCK) SS_TAP(X_SCROLLLOCK));
-        layer_invert(_WFL);
+        layer_invert(1);
+	backlight_set(8);
     return false; break;
     case M_WIN:
 	SEND_STRING(SS_TAP(X_SCROLLLOCK) SS_TAP(X_SCROLLLOCK));
-	layer_invert(_MBL);
+	layer_invert(0);
+	backlight_set(2);
     return false; break;
     }
   }
@@ -59,81 +53,93 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _MBL: (Mac Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Esc |
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;| '|Return|TG(FL)|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Ctrl|Alt |CMD |        Space          |CMD| FN| Alt|Lef|Dow|Rig |
+   * |Ctrl|Alt |CMD |        Space         |CMD|M_Mac|Alt|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
 [_MBL] = KEYMAP_ANSI(
-  KC_ESC, TD(TD_F1),TD(TD_F2),TD(TD_F3),TD(TD_F4),TD(TD_F5),TD(TD_F6),TD(TD_F7),TD(TD_F8),TD(TD_F9),TD(TD_F10), TD(TD_F11), TD(TD_F12), KC_BSPC,KC_GRV,\
-  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,  \
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,        KC_QUOT,       KC_ENT,  KC_PGUP, \
-  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_UP,   KC_PGDN, \
-  KC_LCTL, KC_LALT, KC_LGUI,                         KC_SPC,                                   KC_RGUI, M_MAC  ,KC_RALT,   KC_LEFT, KC_DOWN,  KC_RGHT),
+  KC_GRV, KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , KC_6 ,TD(TDM_7),TD(TDM_8),TD(TDM_9),TD(TDM_10), TD(TDM_11), TD(TDM_12), KC_BSPC,KC_ESC,\
+  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,  \
+  KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,      KC_QUOT,       KC_ENT,  TG(2), \
+  KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_UP,   KC_PGDN, \
+  KC_LCTL, KC_LALT, KC_LGUI,               KC_SPC,                          KC_RGUI, M_MAC  ,KC_RALT,   KC_LEFT, KC_DOWN,  KC_RGHT),
 
-
+ 
   /* Keymap _WFL: (Windows Function Layer) 
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Esc |
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;| '|Return|TG(FL)|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Win|Alt |Ctrl |        Space          |Ctrl| FN| Alt|Lef|Dow|Rig|
+   * |Win|Alt |Ctrl |        Space         |Ctrl|M_WIN|Alt|Lef|Dow|Rig|
    * `----------------------------------------------------------------'
    */
 
 [_WFL] = KEYMAP_ANSI(
-  KC_ESC, TD(TD_F1),TD(TD_F2),TD(TD_F3),TD(TD_F4),TD(TD_F5),TD(TD_F6),TD(TD_F7),TD(TD_F8),TD(TD_F9),TD(TD_F10), TD(TD_F11), TD(TD_F12), KC_BSPC,KC_GRV,\
-  KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS, KC_DEL, \
-  KC_CAPS,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,  KC_PGUP,\
-  KC_LSFT,           KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,      KC_RSFT,  KC_UP,   KC_PGDN,\
-  KC_LGUI,   KC_LALT,  KC_LCTL,                        KC_SPC,                          KC_RCTL, M_WIN   ,   KC_RALT,       KC_LEFT, KC_DOWN, KC_RGHT),
-};
+  KC_GRV, KC_1 , KC_2 , KC_3 ,  KC_4 , KC_5 , KC_6 ,TD(TDW_7),TD(TDW_8),TD(TDW_9),TD(TDW_10), TD(TDW_11), TD(TDW_12), KC_BSPC,KC_ESC,\
+  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS, KC_DEL, \
+  KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,       KC_ENT     ,  _______,\
+  KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,      KC_COMM,  KC_DOT,  KC_SLSH,      KC_RSFT,  KC_UP,   KC_PGDN,\
+  KC_LGUI,   KC_LALT,  KC_LCTL,             KC_SPC,                   KC_RCTL, M_WIN   ,   KC_RALT,       KC_LEFT, KC_DOWN, KC_RGHT),
+
+ 
+  /* Keymap _FL: (Functional Key Layer) 
+   * ,----------------------------------------------------------------.
+   * |   |F1| F2| F3| F4| F5| F6| F7| F8| F9| F10| F11| F12|    |RESET|
+   * |----------------------------------------------------------------|
+   * |    |   |   |   |   |   |   |   |   |   |   |   |   |     |     |
+   * |----------------------------------------------------------------|
+   * |       |   |   |   |   |   |   |   |   |   |   |   |       |    |
+   * |----------------------------------------------------------------|
+   * |        |   |   |BL_DEC|BL_TG|BL_INC|  |  | |  | |BTN1|MS_U|BTN2|
+   * |----------------------------------------------------------------|
+   * |   |    |     |                      |   |   |   |MS_L|MS_D|MS_R|
+   * `----------------------------------------------------------------'
+   */
+
+[_FL] = KEYMAP_ANSI(
+ _______, KC_F1 ,KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10 , KC_F11 , KC_F12 , _______, RESET,\
+ _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, _______,_______,\
+ _______,  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        \
+ _______,   _______,_______,BL_DEC, BL_TOGG,BL_INC, _______,_______,_______, _______,  _______, KC_BTN1, KC_MS_U, KC_BTN2, \
+ _______,_______,_______,                 _______,               _______, _______, _______,   KC_MS_L,   KC_MS_D,  KC_MS_R),
+  };
 
 // Tapdance definitions                                                                                                                               
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_F1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
-  [TD_F2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
-  [TD_F3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
-  [TD_F4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
-  [TD_F5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
-  [TD_F6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F6),
-  [TD_F7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F7),
-  [TD_F8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F8),
-  [TD_F9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F9),
-  [TD_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F10),
-  [TD_F11] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_F11),
-  [TD_F12] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_F12),
-};
-
-/*  
-// Macros
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  if (record->event.pressed) {
-    switch(id) {
-
-    case 0:
-         return MACRO(T(SLCK), W(1), T(SLCK), W(1), TG(_WFL), END);
-
-    case 1:
-         return MACRO(T(SLCK), W(1), T(SLCK), W(1), TG(_MBL), END);
-
-    }
-
-
-    }
-  return MACRO_NONE;
-
-};
-*/
+  //[TDM_1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_1),
+  //[TDM_2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_2),
+  //[TDM_3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_3),
+  //[TDM_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_4),
+  //[TDM_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_5),
+  //[TDM_6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_6),
+  [TDM_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_MRWD),
+  [TDM_8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_MPLY),
+  [TDM_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_MFFD),
+  [TDM_10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_MUTE),
+  [TDM_11] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_VOLD),
+  [TDM_12] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_VOLU),
+  //[TDW_1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_1),
+  //[TDW_2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_2),
+  //[TDW_3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_3),
+  //[TDW_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_4),
+  //[TDW_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_5),
+  //[TDW_6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_6),
+  [TDW_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_MPRV),
+  [TDW_8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_MPLY),
+  [TDW_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_MNXT),
+  [TDW_10] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_MUTE),
+  [TDW_11] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_VOLD),
+  [TDW_12] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_VOLU),
+}; 
